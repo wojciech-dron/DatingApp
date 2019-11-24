0727108c8a19eee32db07a7c3bac0e43d9574fcf
kombinator71 <42037834+kombinator71@users.noreply.github.com>
2019-11-24 23:51:48 +0100

    Initial Commit

---
+++ DatingApp.api/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
