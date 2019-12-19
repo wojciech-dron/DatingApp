156ab37cfe1a0529393d96b58a2a5104206526f0
kombinator71 <42037834+kombinator71@users.noreply.github.com>
2019-12-19 22:21:22 +0100

    Lesson75

--- DatingApp.api/Data/DataContext.cs
+++ DatingApp.api/Data/DataContext.cs
@@ -15,5 +15,6 @@ namespace DatingApp.api.Data
         }
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
+        // public DbSet<Photo> Photos { get; set; }
     }
 }
