434a42bb93a3e2cb6362b10cdff7bdd654e05a87
kombinator71 <42037834+kombinator71@users.noreply.github.com>
2019-12-19 23:00:15 +0100

    Lesson75

---
+++ DatingApp.api/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using DatingApp.api.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IDatingRepository _repo;
+
+        public UsersController(IDatingRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers() 
+        {
+            var users = await _repo.GetUsers();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _repo.GetUser(id);
+
+            return Ok(user);
+        }
+    }
+}
--- DatingApp.api/Migrations/20191219214336_ExtendedUserClass.cs
+++ DatingApp.api/Migrations/20191219214336_ExtendedUserClass.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace DatingApp.api.Migrations
