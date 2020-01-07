25a40caa3cccd96f88dc135d74625e6e217fd8b4
kombinator71 <42037834+kombinator71@users.noreply.github.com>
2020-01-07 23:31:02 +0100

    End of section 9

--- DatingApp-spa/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-spa/src/app/members/member-detail/member-detail.component.ts
@@ -27,7 +27,7 @@ export class MemberDetailComponent implements OnInit {
 
     this.galleryOptions = [{
       width: '500px',
-      height: '500px:',
+      height: '500px',
       imagePercent: 100,
       thumbnailsColumns: 4,
       imageAnimation: NgxGalleryAnimation.Slide,
